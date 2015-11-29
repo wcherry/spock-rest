f3ed67ffe309f96977d676f9c0738512de75b26f
William Cherry <wcherry69@gmail.com>
2015-11-29 15:40:12 -0800

    Added code to spec to run test for people REST service. Added wslite Groovy Rest/Soap library to provide Rest connection support.

--- src/test/groovy/SpockRestSpec.groovy
+++ src/test/groovy/SpockRestSpec.groovy
@@ -1,13 +1,17 @@
 import spock.lang.Specification
+import wslite.rest.RESTClient
+import wslite.rest.Response
 
 class SpockRestSpec extends Specification{
 
     def "Should return a person"(){
-        given: "userid 0"
-
-        when:  "we request it from the server"
+        given: "we request it from the server"
+            RESTClient client = new RESTClient('http://localhost:8080/')
+        when:  "userid 0"
+            Response resp = client.get([path: "/people/0"])
 
         then: "we should have a successful response"
+            resp.statusCode == 200
 
         and: "the person matches our expectations"
 
