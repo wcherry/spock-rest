88fd3a3b12aef2fe5f222dec3ae1e8c2ad69e668
William Cherry <wcherry69@gmail.com>
2015-11-29 19:04:01 -0800

    Added test for json content.

--- src/test/groovy/SpockRestSpec.groovy
+++ src/test/groovy/SpockRestSpec.groovy
@@ -4,15 +4,17 @@ import spock.lang.Specification
 class SpockRestSpec extends Specification implements RestTrait{
     def "Should return a person"(){
         given: "we request it from the server"
-        url = 'http://localhost:8080/'
+            url = 'http://localhost:8080/'
 
         when:  "userid 0"
-        def resp = get([path: "/people/0"])
+            def resp = get([path: "/people/0"])
 
         then: "we should have a successful response"
-        resp.statusCode == 200
+            resp.statusCode == 200
+            resp.contentType == "application/json"
 
         and: "the person matches our expectations"
-
+            resp.json.fname == 'william'
+            resp.json.lname == 'cherry'
     }
 }
